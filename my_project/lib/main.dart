449a658c733e51659fcaf6d9158f01cdea0737f9
Ashish Zapadiya <sirzapadiya@gmail.com>
2022-09-06 13:52:28 +0530

    My_Project Created.

---
+++ my_project/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  int ashish = 10;
+  double a = 10.22;
+  var as = "Ashish";
+  num ass = 1099.22;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Container(
+          child: Text("Ashish $a, \n$as, $ashish, $ass."),
+        ),
+      ),
+    );
+  }
+}
